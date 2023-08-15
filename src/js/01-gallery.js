af6dd4afaae3b244e65692914bc200ce17e00264
kulikiliya <136320307+kulikiliya@users.noreply.github.com>
2023-08-15 16:38:38 +0300

    first try

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const galleryItemsString = galleryItems.map( item =>
+    `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}"" alt="${item.description}" />
+   </a>
+</li>`
+).join("");
+
+galleryList.innerHTML += galleryItemsString;
+
+new SimpleLightbox('.gallery__item a', { 
+    captions: true,
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: `bottom`
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+
+form.addEventListener('input', throttle(onInputData, 1000));
+form.addEventListener('submit',onSubmitForm)
+
+const formData = {};
+
+function onInputData(el) {
+    formData[el.target.name] = el.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+    console.log(formData);
+}
+
+function onSubmitForm(el) {
+
+    el.preventDefault();
+    
+      if (email.value === "" || message.value === "") {
+        return alert("Please, fill the form")
+    };
+  
+  console.log(formData)
+  el.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+  
+}
+
+
+function fromLocalStorage() {
+    
+    const storage = JSON.parse(localStorage.getItem('feedback-form-state'));
+    
+
+    if (storage) {
+        // добавить в объект с данными - инфу с локального хранилища, чтобы не вылетал один из импутов при обновлении страницы
+        formData.email = storage.email;
+        formData.message = storage.message;
+
+        email.value = storage.email
+        message.value = storage.message
+    } 
+};
+
+fromLocalStorage();
+ 
