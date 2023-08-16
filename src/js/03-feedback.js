c1f47f7f7bbeacbe21701c46595aab632a3e723e
kulikiliya <136320307+kulikiliya@users.noreply.github.com>
2023-08-16 10:17:01 +0300

    Udated 03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,41 +7,45 @@ const message = document.querySelector('.feedback-form textarea');
 form.addEventListener('input', throttle(onInputData, 1000));
 form.addEventListener('submit',onSubmitForm)
 
-const formData = {};
+let formData = {};
 
 function onInputData(el) {
+  // значения свойств объекта равны значением нашего инпута .
     formData[el.target.name] = el.target.value;
+  // кидаем полученные данные из оюъекта в localStorage.
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
 function onSubmitForm(el) {
 
     el.preventDefault();
-    
+    // проверка на ввод
       if (email.value === "" || message.value === "") {
         return alert("Please, fill the form")
     };
   
   console.log(formData)
-  el.currentTarget.reset();
+  // Чистим localStorage.
   localStorage.removeItem('feedback-form-state');
-  
+  // Чистим инпут.
+  el.currentTarget.reset();
+  // НЕ ЗАБЫТЬ ОЧИСТИТЬ ОБЪЕКТ .
+  formData = {};
 }
 
 
 function fromLocalStorage() {
     
-    const storage = JSON.parse(localStorage.getItem('feedback-form-state'));
+  let storage = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
     
-
-    if (storage) {
-        // добавить в объект с данными - инфу с локального хранилища, чтобы не вылетал один из импутов при обновлении страницы
-        formData.email = storage.email;
-        formData.message = storage.message;
-
-        email.value = storage.email
-        message.value = storage.message
-    } 
+  // if (storage) не нужен.
+  // добавить в объект с данными - инфу с локального хранилища, чтобы не вылетал один из импутов при обновлении страницы.
+  // добавили логический оперотор для того , чтобы убрать undefinde.
+      formData.email = storage.email || '';
+      formData.message = storage.message || '';
+  
+      email.value = storage.email || '';
+      message.value = storage.message || '';
 };
 
 fromLocalStorage();
